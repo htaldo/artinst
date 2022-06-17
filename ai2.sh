bb22351e08b2042ce4de7310a7a4fc627cbad5c3
htaldo <64457490+htaldo@users.noreply.github.com>
2022-06-17 12:25:55 -0500

    Update ai2.sh

--- ai2.sh
+++ ai2.sh
@@ -6,13 +6,17 @@ echo "STAGE 2: POST-INSTALL"
 #mv ~/artinst/fonts/* /usr/share/fonts
 sv up dhcpcd-runit
 sv up iwd-runit
-iwctl station wlan0 scan
-iwctl station wlan0 connect "$network"
+
+#iwctl station wlan0 scan
+#iwctl station wlan0 connect "$network"
 
 doas pacman --noconfirm -Rsn sudo
 doas pacman --noconfirm -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot xorg-xhost
-doas pacman --noconfirm -S rust cargo python3 go fzf file acpi patch xdg-utils pulse-audio alsa-utils alsa-utils-doc alsa-lib alsaconf alsa-ucm-conf pamixer xclip maim zathura mpv zathura-djvu zathura-pdf-mupdf newsboat calcurse pfetch neofetch bat tmux cmus picom rxvt-unicode inkscape libreoffice krita blender
-doas pacman --noconfirm -S gcc g++ libx11 libxft libxinerama ncurses dbus-x11 adwaita-gtk3-theme adwaita-icon-theme ttf-dejavu gtk3 setxkbmap video-intel-x68 light udisks2 dbus feh
+doas pacman --noconfirm -S rust cargo python3 go fzf file acpi patch xdg-utils 
+doas pacman --noconfirm -S pulse-audio alsa-utils alsa-utils-doc alsa-lib alsaconf alsa-ucm-conf pamixer 
+doas pacman --noconfirm -S xclip maim zathura mpv zathura-djvu zathura-pdf-mupdf newsboat calcurse pfetch neofetch bat tmux cmus picom rxvt-unicode 
+doas pacman --noconfirm -S inkscape libreoffice krita blender
+doas pacman --noconfirm -S gcc g++ libx11 libxinerama libxft ncurses dbus-x11 adwaita-gtk3-theme adwaita-icon-theme ttf-dejavu gtk3 setxkbmap video-intel-x68 light udisks2 dbus feh
 
 #scripts
 mkdir ~/.local
@@ -50,6 +54,7 @@ cd ..
 rm dmenu-5.0.tar.gz
 
 #configs
+mkdir ~/.config ~/.newsboat
 cp ~/artinst/configs/.bashrc ~/.bashrc
 cp ~/artinst/configs/.bash_profile ~/.bash_profile
 cp ~/artinst/configs/.xinitrc ~/.xinitrc
