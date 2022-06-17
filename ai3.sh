d40ab4be0bb70c9a3268c5b7c5203bf385580d5f
htaldo <64457490+htaldo@users.noreply.github.com>
2022-06-17 17:49:11 -0500

    Update ai3.sh

--- ai3.sh
+++ ai3.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 echo "STAGE 3: EXTRA"
-doas pacman --noconfirm -S rust cargo python3 go fzf inkscape libreoffice krita blender bat pfetch
+doas pacman --noconfirm -S rust cargo python3 go fzf inkscape libreoffice krita blender bat 
+
+#paru
+cd ~/.local/src
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si 
+sed -i '[bin]/s/^#//' /etc/paru.conf
+sed -i 'Sudo=doas/s/^#//' /etc/paru.conf
+
 echo 1 | paru simple-mtpfs 
 echo 1 | paru lf 
+echo 1 | paru pfetch
 echo 1 | paru librewolf-bin 
 echo 1 | paru betterlockscreen 
 echo 1 | paru yt-dlp 
