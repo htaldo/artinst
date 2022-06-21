701cb91767f950a9f2a17a536202181bc2ef0693
htaldo <64457490+htaldo@users.noreply.github.com>
2022-06-20 23:16:46 -0500

    Update aux.sh

--- aux.sh
+++ aux.sh
@@ -28,13 +28,15 @@ sed -i '/\[lib32\]/s/^#//' /etc/pacman.conf
 sed -i '/\[lib32\]/{n;s/#//}' /etc/pacman.conf
 echo "
 [universe]
-  Server = https://universe.artixlinux.org/$arch
-  Server = https://mirror1.artixlinux.org/universe/$arch
-  Server = https://mirror.pascalpuffke.de/artix-universe/$arch
-  Server = https://artixlinux.qontinuum.space/artixlinux/universe/os/$arch
-  Server = https://mirror1.cl.netactuate.com/artix/universe/$arch
-  Server = https://ftp.crifo.org/artix-universe/
-  
+Server = https://universe.artixlinux.org/$arch
+Server = https://mirror1.artixlinux.org/universe/$arch
+Server = https://mirror.pascalpuffke.de/artix-universe/$arch
+Server = https://artixlinux.qontinuum.space/artixlinux/universe/os/$arch
+Server = https://mirror1.cl.netactuate.com/artix/universe/$arch
+Server = https://ftp.crifo.org/artix-universe/" >> /etc/pacman.conf
+pacman -Sy 
+ 
+ echo "
 #Arch
 [extra]
 Include = /etc/pacman.d/mirrorlist-arch
