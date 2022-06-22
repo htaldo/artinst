19d97e4bbc30ff5a514c320865973dfa4a2323ee
htaldo <64457490+htaldo@users.noreply.github.com>
2022-06-22 17:58:29 +0000

    Update th.sh

--- bin/th.sh
+++ bin/th.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 homdir='/home/aldo'
+thdir='/home/aldo/.local/bin/res/th'
 
 #change .Xresources
-sed -i -f $homdir/fl/sc/res/th/$1/xres $homdir/.Xresources
+sed -i -f $thdir/$1/xres $homdir/.Xresources
 #change dwm
-sed -i -f $homdir/fl/sc/res/th/$1/dwm $homdir/src/dwm-6.2/config.h
-cd $homdir/src/dwm-6.2; make clean install
+sed -i -f $thdir/$1/dwm $homdir/.local/src/dwm-6.2/config.h
+cd $homdir/.local/src/dwm-6.2; make clean install
 #change dmenu
-sed -i -f $homdir/fl/sc/res/th/$1/dmenu $homdir/src/dmenu-5.0/config.h
-cd $homdir/src/dmenu-5.0; make clean install
+sed -i -f $thdir/$1/dmenu $homdir/.local/src/dmenu-5.0/config.h
+cd $homdir/.local/src/dmenu-5.0; make clean install
 #change .vimrc
-sed -i -f $homdir/fl/sc/res/th/$1/vimrc $homdir/.vimrc
+sed -i -f $thdir/$1/vimrc $homdir/.vimrc
 #change zathurarc
-sed -i -f $homdir/fl/sc/res/th/$1/zathura $homdir/.config/zathura/zathurarc
+sed -i -f $thdir/$1/zathura $homdir/.config/zathura/zathurarc
 #change wallpaper
-sed -i -f $homdir/fl/sc/res/th/$1/xinitrc $homdir/.xinitrc
+sed -i -f $thdir/$1/xinitrc $homdir/.xinitrc
