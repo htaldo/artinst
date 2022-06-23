00e0a940d1a09f76bd1c92c2018bb6766195b3d9
htaldo <64457490+htaldo@users.noreply.github.com>
2022-06-23 02:16:57 +0000

    Update config.h

--- configs/dwm-6.2/config.h
+++ configs/dwm-6.2/config.h
@@ -44,7 +44,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Alma Mono:size=10:antialias=true", "JoyPixels:size=10" };
+static const char *fonts[]          = { "Alma Mono:size=10:antialias=true" };
 static const char dmenufont[]       = "Alma Mono:size=10:antialias=true";
 static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#282828";
