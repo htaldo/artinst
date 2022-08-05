e2c7abda3ce669f5a5faecf3e57a8a25037626ab
htaldo <64457490+htaldo@users.noreply.github.com>
2022-08-05 02:57:15 +0000

    Update sr.sh

--- bin/sr.sh
+++ bin/sr.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-procs="$(pgrep sr.sh)"
+#sr - screen record
+procs="$(pgrep sr)"
 if [ "$(echo "$procs" | wc -l)" -gt 1 ]; then
 	#~/fl/sc/notif.sh sr: stopping recording...
 	othproc="$(echo "$procs" | grep -v $$)"
@@ -8,7 +9,7 @@ if [ "$(echo "$procs" | wc -l)" -gt 1 ]; then
 else	
 	srdir=~/vid
 	cd $srdir
-	name="$(~/fl/sc/sd.sh)"
+	name="$(~/fl/sc/sd)"
 	#(~/fl/sc/sb.sh "$(echo o rec)")&
 	ffmpeg -f x11grab -framerate 25 -s 1366x768 -i :0.0 -f pulse -ac 2 -i 0 "$name.mkv" 
 fi
