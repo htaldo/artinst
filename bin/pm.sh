6899001297e85a01a6603dbce9ca3e2df29b0873
htaldo <64457490+htaldo@users.noreply.github.com>
2022-08-05 02:52:30 +0000

    Update pm.sh

--- bin/pm.sh
+++ bin/pm.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
+#pm - powermenu
 select="$(echo -e '1)shutdown\n2)lock\n3)reboot\n4)flush\n9)suspend' | dmenu)"
 case $select in
 	"1)shutdown")
 		doas /sbin/poweroff
 	;;
 	"2)lock")
-		betterlockscreen -l
+		betterlockscreen -l blur
 	;;
 	"3)reboot")
 		doas /sbin/reboot
 	;;
 	"4)flush")
-		sync; echo 3 | doas tee /proc/sys/vm/drop_caches
+		sync;  echo 3 > doas tee /proc/sys/vm/drop_caches
 	;;
 	"9)suspend")
 		echo deep | doas /usr/bin/tee /sys/power/mem_sleep
