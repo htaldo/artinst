ac36b308a96c3c0470811b68f56cf97e1e156f5e
htaldo <64457490+htaldo@users.noreply.github.com>
2022-08-05 02:43:55 +0000

    Update and rename bb.sh to bm

--- bin/bb.sh
+++
@@ -1,5 +0,0 @@
-query="$(echo -e '\c' | dmenu -p "open bookmarks:")"
-
-if [ -n "$query" ]; then
-	select="$(fzf -f "$query" < ~/.local/bin/res/bb.txt | dmenu -l 10)" && librewolf "$(echo "$select" | sed 's/^.* //')"
-fi
